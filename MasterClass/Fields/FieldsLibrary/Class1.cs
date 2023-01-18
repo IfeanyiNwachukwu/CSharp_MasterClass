d57e506244a8b094a5df2d3e0a29d214f0db2739
Ifeanyi Nwachukwu <ifenwachukwu7@outlook.com>
2023-01-19 00:26:27 +0100

    completed Fields

---
+++ MasterClass/Fields/FieldsLibrary/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FieldsLibrary
+{
+    public class Product
+    {
+        //fields
+        public int productID;
+        public string productName;
+        public double cost;
+        public int quantityInStock;
+        public static int TotalNoProducts;
+        public const string CategoryName = "Electronics";
+
+        public readonly string dateOfPurchase;
+
+        public Product()
+        {
+            dateOfPurchase = DateTime.Now.ToShortDateString();
+        }
+    }
+}
